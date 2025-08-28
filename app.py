e084085f7c8a8f29bec73566ff35dec8853ec99b
Anchalbhoite <121814149+Anchalbhoite@users.noreply.github.com>
2025-08-28 17:46:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,11 +65,13 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+   st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     st.write("🔍 Predicting...")
     model = load_model()
     label = predict_image(model, image)
 
     st.success(f"✅ Predicted Animal: **{label}**")
+
 
